81404d1f05b3b2ef8016c14a962240fa0ca5ca28
Wjdan <wejdan10x@gmail.com>
2021-07-14 18:24:03 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import ExpItem from "./components/ExpItem";
 import './App.css';
 
+//JSX file html inside JS
+// const app = () =>{} instead of function
 function App() {
+  //array of object
+  const expenses = [{
+    title: 'Grosry', amount: '260', date: new Date(2021, 4, 1)
+  },
+    {title: 'car ins', amount: '290', date: new Date(2021, 5, 1)}
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='title'>Apps </h1>
+      <h2 className='title'>Expenses items </h2>
+      {/*components */}
+      <ExpItem title={expenses[0].title}
+               amout={expenses[0].amount}
+               date={expenses[0].date} />
+
+      <ExpItem title={expenses[1].title}
+               amout={expenses[1].amount}
+               date={expenses[1].date} />
+
+
     </div>
   );
 }
